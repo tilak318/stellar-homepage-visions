043fe224c1bcfaff88104dc294524b0e9494efbd
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-06-09 10:19:56 +0530

    Your commit message here

--- src/components/NicheSection.tsx
+++ src/components/NicheSection.tsx
@@ -55,7 +55,7 @@ const NicheSection = () => {
             Where We Help To Automate This Business
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8 justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 justify-center">
           {niches.map((niche, index) => (
             <div
               key={index}
